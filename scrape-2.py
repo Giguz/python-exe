1730e9a29abf706c9c5fb29f68db1b60632adba8
Giguz <g.gualandi@evoluzionetelematica.it>
2018-03-19 17:09:50 +0100

    5

--- scrape-2.py
+++ scrape-2.py
@@ -2,33 +2,47 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
-csv_file = open('ric_correlate.csv','w')
-csv_writer = csv.writer(csv_file)
+
+
+def getkw(url):
+	source = requests.get(url).text
+	soup = BeautifulSoup(source, 'lxml')
+
+	mydivs = soup.findAll('p',class_='aw5cc')
+
+	for eachp in mydivs:
+		return eachp
 
 
 
-source = requests.get('https://www.google.it/search?q=bomboniere').text
-soup = BeautifulSoup(source, 'lxml')
+def gethttp(url):
+	source = requests.get(url).text
+	soup = BeautifulSoup(source, 'lxml')
+	mydivs = soup.findAll('p',class_='aw5cc')
 
-mydivs = soup.findAll('p',class_='aw5cc')
-
-for eachp in mydivs:
-	print("https://www.google.it" + eachp.a['href'])
-	csv_writer.writerow([eachp.text])
-
-
-csv_file.close()
+	for eachp in mydivs:
+		print("https://www.google.it" + eachp.a['href'])
+		
 
 def gotourl(url):
 	newurl = requests.get('url').text
 	return newurl
 
 # def listurl(url):
-# 	source = requests.get('https://www.google.it/search?q=bomboniere').text
+# 	source = requests.get('url').text
 # 	soup = BeautifulSoup(source, 'lxml')
 
 # 	mydivs = soup.findAll('p',class_='aw5cc')
 
 # 	for eachp in mydivs:
 # 		print(eachp)
+# csv_file = open('ric_correlate.csv','w')
+# csv_writer = csv.writer(csv_file)
+
+#csv_writer.writerow([eachp.text])
+#csv_file.close()
+#
 	
+getkw("https://www.google.it/search?q=bomboniere")		
+gethttp('https://www.google.it/search?q=bomboniere')
+
