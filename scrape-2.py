1535d81f9864deaf17fc228eae88da78c19ed2a2
Giguz <g.gualandi@evoluzionetelematica.it>
2018-03-19 16:15:00 +0100

    4

--- scrape-2.py
+++ scrape-2.py
@@ -1,13 +1,34 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
+
+csv_file = open('ric_correlate.csv','w')
+csv_writer = csv.writer(csv_file)
+
 
 
 source = requests.get('https://www.google.it/search?q=bomboniere').text
-
 soup = BeautifulSoup(source, 'lxml')
-oksoup = soup.prettify()
 
 mydivs = soup.findAll('p',class_='aw5cc')
 
 for eachp in mydivs:
-	print(eachp.text)
+	print("https://www.google.it" + eachp.a['href'])
+	csv_writer.writerow([eachp.text])
+
+
+csv_file.close()
+
+def gotourl(url):
+	newurl = requests.get('url').text
+	return newurl
+
+# def listurl(url):
+# 	source = requests.get('https://www.google.it/search?q=bomboniere').text
+# 	soup = BeautifulSoup(source, 'lxml')
+
+# 	mydivs = soup.findAll('p',class_='aw5cc')
+
+# 	for eachp in mydivs:
+# 		print(eachp)
+	
